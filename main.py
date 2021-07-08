ee7fefe3e76a6b2f189c07e74c98ff7d7530a23d
ManojBaasha <manoj.elango123@gmail.com>
2021-07-08 16:05:03 -0700

    added a few comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#! usr/bin/python3
 import discord
 import os
 
@@ -9,6 +10,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 with open('token.txt') as f:
+    """ using a text file to store the discord bot token"""
     token = f.read()
         
 client.run(token)
